0707d31d8c26963aa54d151da0c9772ce4a972d5
jasminjeminez <148034328+jasminjeminez@users.noreply.github.com>
2023-10-25 05:57:19 +0100

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,16 @@
+/* Jeminez, Jasmin
+   BSIT 2
+   Sept 13, 2023
+*/
+
+class FavoriteSong
+{
+     public static void main(String[] args)
+     {
+     System.out.println("Oh, love");
+     System.out.println("Never knew what I was missin'");
+     System.out.println("But I knew once we start kissin");
+     System.out.println("I found love");
+     }
+}     
+
